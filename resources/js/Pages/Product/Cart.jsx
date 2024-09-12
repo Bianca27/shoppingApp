53a65d77b85d59d186afff7ee6c57751acb7fe0f
biancaradu <bianca.radu@dreamproduction.com>
2024-09-12 16:25:49 +0300

    make checkout disabled if there are no cartitems

--- resources/js/Pages/Product/Cart.jsx
+++ resources/js/Pages/Product/Cart.jsx
@@ -164,8 +164,13 @@ export default function Cart({ auth, cartItems, total }) {
                             </div>
 
                             <button
-                                className="flex w-full items-center justify-center rounded-lg bg-blue-500 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300"
+                                className={`flex w-full items-center justify-center rounded-lg px-5 py-2.5 text-sm font-medium text-white focus:outline-none focus:ring-4 ${
+                                    cartItems.length === 0
+                                        ? 'bg-gray-500 cursor-not-allowed'
+                                        : 'bg-blue-500 hover:bg-blue-600 focus:ring-blue-300'
+                                }`}
                                 onClick={checkout}
+                                disabled={cartItems.length === 0}
                             >
                                Checkout
                             </button>
