8e1b37f192d5937b5eb4c9ede52fa5924737d4f4
biancaradu <bianca.radu@dreamproduction.com>
2024-09-12 15:51:44 +0300

    add seeder for products

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Image;
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Product::create([
+            'name' => 'Nike Shoes',
+            'price' => 100,
+            'description' => 'This is the first product',
+            'user_id' => 1,
+            ],
+        );
+
+        Product::create([
+            'name' => 'Puma shoes',
+            'price' => 200,
+            'description' => 'This is the second product',
+            'user_id' => 1,
+        ]);
+        Product::create([
+            'name' => 'New balance shoes',
+            'price' => 300,
+            'description' => 'This is the third product',
+            'user_id' => 2,
+        ]);
+    }
+}
